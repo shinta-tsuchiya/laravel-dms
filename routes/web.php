a791dbae8ab90e0c0fc6eae902683f7ce83a0dc5
shintrb <[tsuchiyashinta@gmail.com]>
2020-06-06 17:25:38 +0900

    元に戻した

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
 Auth::routes();
-Route::get('/', 'HomeController@index');
-Route::get('/documents', 'DocumentController@index')->name('documents.index');
 
+Route::get('/', 'DocumentController@index');
